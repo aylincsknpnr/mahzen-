fd8a0ab0d09238fed0e22f539beb9a0281ceb697
aylincsknpnr <aylin_comu@hotmail.com>
2014-05-17 02:11:18 +0300

    Update rastgeleUretec.rb

--- rastgeleUretec.rb
+++ rastgeleUretec.rb
@@ -12,18 +12,14 @@ tane2=satir[1].to_i
 tane3=satir[2].to_i
 tane4=satir[3].to_i
 tane5=satir[4].to_i
-
-
 def time_rand
-        from = 0.0
-        to = Time.now
+	from = 0.0
+    	to = Time.now
         Time.at(from + rand * (to.to_f - from.to_f))
 end
-
 $mail=""
 array=""
 uzanti=["@hotmail.com", "@gmail.com"]
-
 while index<okunan2
       i=0
       while i<tane2
@@ -44,7 +40,7 @@ while index<okunan2
             tarih=time_rand
 	    print tarih,"\n"
             k=k+1
-       end
+      end
       l=0 
       while l<tane5
              $mail=(0...25).map{ ('a'..'z').to_a[rand(29)] }.join
@@ -53,5 +49,4 @@ while index<okunan2
              l=l+1
       end     
 index=index+1
-       
 end    
